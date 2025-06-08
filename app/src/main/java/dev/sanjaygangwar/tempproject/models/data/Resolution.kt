ca6cb9bbb9fc140931daaf113d24eefce1bbc356
Sanjay S Gangwar <contact.gangwar@gmail.com>
2025-06-08 13:05:42 +0530

    Handled 1.1 done

---
+++ app/src/main/java/dev/sanjaygangwar/tempproject/models/data/Resolution.kt
@@ -0,0 +1,9 @@
+package dev.sanjaygangwar.tempproject.models.data
+
+data class Resolution(
+    val label: String,
+    val width: Int,
+    val height: Int,
+    val id: String,
+    val bandwidth: Int
+)
--- app/src/main/java/dev/sanjaygangwar/tempproject/ui/fragment/home/Home.kt
+++ app/src/main/java/dev/sanjaygangwar/tempproject/ui/fragment/home/Home.kt
@@ -2,6 +2,8 @@ package dev.sanjaygangwar.tempproject.ui.fragment.home
 
 import android.content.pm.ActivityInfo
 import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.annotation.OptIn
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
@@ -16,29 +18,36 @@ import androidx.media3.common.util.UnstableApi
 import androidx.media3.datasource.DefaultDataSource
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.exoplayer.source.DefaultMediaSourceFactory
+import androidx.media3.exoplayer.trackselection.DefaultTrackSelector
 import androidx.media3.ui.AspectRatioFrameLayout
 import androidx.media3.ui.PlayerControlView
 import androidx.media3.ui.PlayerView
 import dagger.hilt.android.AndroidEntryPoint
 import dev.sanjaygangwar.tempproject.R
 import dev.sanjaygangwar.tempproject.databinding.HomeBinding
+import dev.sanjaygangwar.tempproject.models.data.Resolution
+import dev.sanjaygangwar.tempproject.models.data.VideoModelData
 import dev.sanjaygangwar.tempproject.ui.base.BaseFragment
 import dev.sanjaygangwar.tempproject.utils.ToastUtil.mLog
 import dev.sanjaygangwar.tempproject.utils.ToastUtil.mToast
 import dev.sanjaygangwar.tempproject.utils.extenstionfuntions.ImageExtensions.hide
 import dev.sanjaygangwar.tempproject.utils.extenstionfuntions.ImageExtensions.show
 import dev.sanjaygangwar.tempproject.utils.network.retrofit.Resource
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import java.net.HttpURLConnection
+import java.io.ByteArrayInputStream
 import java.net.URL
+import javax.xml.parsers.DocumentBuilderFactory
 
 
+@UnstableApi
 @AndroidEntryPoint
 class Home : BaseFragment<HomeBinding>(HomeBinding::inflate) {
 
     private lateinit var player: ExoPlayer
+    private lateinit var trackSelector: DefaultTrackSelector
     private val viewModel: HomeViewModel by viewModels()
     private var isZoomed = false
 
@@ -59,20 +68,7 @@ class Home : BaseFragment<HomeBinding>(HomeBinding::inflate) {
                 }
 
                 Resource.Status.SUCCESS -> {
-                    bind?.progressBar?.hide()
-                    val data = value.data?.data
-                    if (data?.mediaUrl.isNullOrEmpty() || data?.licenseUri?.isEmpty()!!) {
-                        bind?.playerView?.hide()
-                        context?.mToast("No video available")
-                        return@observe
-                    } else {
-                        bind?.playerView?.show()
-                    }
-
-                    lifecycleScope.launch {
-                        fetchVideoData(data.mediaUrl)
-                    }
-                    setupExoplayer(data.licenseUri, data.mediaUrl, bind?.playerView)
+                    handleVideoData(value)
                 }
 
                 Resource.Status.ERROR -> {
@@ -83,18 +79,91 @@ class Home : BaseFragment<HomeBinding>(HomeBinding::inflate) {
         }
     }
 
-    suspend fun fetchVideoData(mediaUrl: String) {
+    private fun handleVideoData(value: Resource<VideoModelData>?) {
+
+        bind?.progressBar?.hide()
+        val data = value?.data?.data
+        if (data?.mediaUrl.isNullOrEmpty() || data?.licenseUri?.isEmpty()!!) {
+            bind?.playerView?.hide()
+            context?.mToast("No video available")
+            return
+        } else {
+            bind?.playerView?.show()
+        }
+
+        lifecycleScope.launch {
+            fetchVideoData(data.mediaUrl, { resolutionList ->
+                // Update your Spinner or Resolution Selector UI
+                val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, resolutionList.map { it.label })
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                bind?.resolutionSpinner?.adapter = adapter
+
+                // Set the initial selection to the first resolution
+                bind?.resolutionSpinner?.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                    override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+                        val selected = resolutionList[position]
+                        if (this@Home::player.isInitialized && this@Home::trackSelector.isInitialized) {
+                            // Update the track selector parameters to set the selected resolution
+                            val parameters = trackSelector.buildUponParameters().setMaxVideoSize(selected.width, selected.height).setMinVideoSize(selected.width, selected.height).build()
+                            trackSelector.parameters = parameters
+                        }
+                    }
+
+                    override fun onNothingSelected(parent: AdapterView<*>) {}
+                }
+            })
+        }
+        // Set up the ExoPlayer with the fetched video data
+        setupExoplayer(data.licenseUri, data.mediaUrl, bind?.playerView)
+    }
+
+    suspend fun fetchVideoData(
+        mediaUrl: String,
+        onResolutionsParsed: (List<Resolution>) -> Unit,
+        ioDispatcher: CoroutineDispatcher = Dispatchers.IO,
+        defaultDispatcher: CoroutineDispatcher = Dispatchers.Default
+    ) {
         try {
-            val xmlString = withContext(Dispatchers.IO) {
-                val connection = URL(mediaUrl).openConnection() as HttpURLConnection
-                val inputStream = connection.inputStream
-                inputStream.bufferedReader().use { it.readText() }
+            // Fetch the MPD XML from the media URL
+            val xmlString = withContext(ioDispatcher) {
+                URL(mediaUrl).openStream().bufferedReader().use { it.readText() }
             }
-            mLog("XML Data: $xmlString")
+            val resolutions = withContext(defaultDispatcher) {
+                parseResolutionsFromMPD(xmlString)
+            }
+            onResolutionsParsed(resolutions)
+
         } catch (e: Exception) {
             e.printStackTrace()
             mLog("Error fetching video data: ${e.message}")
         }
+    }
+
+
+    fun parseResolutionsFromMPD(mpdXml: String): List<Resolution> {
+        // Parse the MPD XML to extract resolutions
+        val resolutions = mutableListOf<Resolution>()
+        val factory = DocumentBuilderFactory.newInstance()
+        val builder = factory.newDocumentBuilder()
+        val inputStream = ByteArrayInputStream(mpdXml.toByteArray())
+        val doc = builder.parse(inputStream)
+
+        // Normalize the document to avoid issues with whitespace
+        val representations = doc.getElementsByTagName("Representation")
+        for (i in 0 until representations.length) {
+            val node = representations.item(i)
+            val attrs = node.attributes
+            val width = attrs.getNamedItem("width")?.nodeValue?.toIntOrNull() ?: continue
+            val height = attrs.getNamedItem("height")?.nodeValue?.toIntOrNull() ?: continue
+            val id = attrs.getNamedItem("id")?.nodeValue ?: continue
+            val bandwidth = attrs.getNamedItem("bandwidth")?.nodeValue?.toIntOrNull() ?: 0
+
+            val label = "${height}p"
+            // Create a Resolution object and add it to the list
+            resolutions.add(Resolution(label, width, height, id, bandwidth))
+        }
+
+        return resolutions.sortedBy { it.height } // Optional: sort ascending
     }
 
     @OptIn(UnstableApi::class)
@@ -104,6 +173,8 @@ class Home : BaseFragment<HomeBinding>(HomeBinding::inflate) {
             player.release()
         }
 
+        context?.let { trackSelector = DefaultTrackSelector(it) }
+
         // Check if licenseUri and mediaUrl are not null or empty
         if (licenseUri.isNullOrEmpty() || mediaUrl.isNullOrEmpty()) {
             context?.mToast("License URI or Media URL is empty")
@@ -112,11 +183,7 @@ class Home : BaseFragment<HomeBinding>(HomeBinding::inflate) {
 
         // Create MediaItem with DRM configuration
         val drmConfiguration = MediaItem.DrmConfiguration.Builder(C.WIDEVINE_UUID).setLicenseUri(licenseUri).build()
-        val mediaItem = MediaItem.Builder()
-            .setUri(mediaUrl)
-            .setDrmConfiguration(drmConfiguration)
-            .setMimeType(MimeTypes.APPLICATION_MPD)
-            .build()
+        val mediaItem = MediaItem.Builder().setUri(mediaUrl).setDrmConfiguration(drmConfiguration).setMimeType(MimeTypes.APPLICATION_MPD).build()
 
         // Initialize ExoPlayer with the MediaItem
         context?.let { context ->
@@ -124,43 +191,40 @@ class Home : BaseFragment<HomeBinding>(HomeBinding::inflate) {
             val mediaSourceFactory = DefaultMediaSourceFactory(dataSourceFactory)
 
             // Create and configure the ExoPlayer instance
-            player = ExoPlayer.Builder(context)
-                .setMediaSourceFactory(mediaSourceFactory)
-                .build()
-                .also { exoPlayer ->
+            player = ExoPlayer.Builder(context).setMediaSourceFactory(mediaSourceFactory).setTrackSelector(trackSelector).build().also { exoPlayer ->
 
-                    exoPlayer.setMediaItem(mediaItem)
-                    exoPlayer.prepare()
-                    exoPlayer.playWhenReady = true
+                exoPlayer.setMediaItem(mediaItem)
+                exoPlayer.prepare()
+                exoPlayer.playWhenReady = true
 
-                    playerView?.player = exoPlayer
-                    toggleImmersiveMode()
-                    playerView?.resizeMode = AspectRatioFrameLayout.RESIZE_MODE_FIT
+                playerView?.player = exoPlayer
+                playerView?.resizeMode = AspectRatioFrameLayout.RESIZE_MODE_FIT
 
-                    playerView?.setControllerVisibilityListener(
-                        PlayerControlView.VisibilityListener { visibility ->
-                            bind?.btnZoom?.visibility = if (visibility == View.VISIBLE) View.VISIBLE else View.GONE
-                        }
-                    )
+                // Set the player view's resize mode to fit
+                playerView?.setControllerVisibilityListener(
+                    PlayerControlView.VisibilityListener { visibility ->
+                        bind?.resolutionSpinner?.visibility = if (visibility == View.VISIBLE) View.VISIBLE else View.GONE
+                        bind?.btnZoom?.visibility = if (visibility == View.VISIBLE) View.VISIBLE else View.GONE
+                    })
 
-                    // --- Add listener to show/hide loader ---
-                    exoPlayer.addListener(object : Player.Listener {
-                        override fun onPlaybackStateChanged(state: Int) {
-                            super.onPlaybackStateChanged(state)
-                            when (state) {
-                                Player.STATE_BUFFERING -> {
-                                    bind?.progressBar?.visibility = View.VISIBLE
-                                }
+                // Add a listener to handle playback state changes
+                exoPlayer.addListener(object : Player.Listener {
+                    override fun onPlaybackStateChanged(state: Int) {
+                        super.onPlaybackStateChanged(state)
+                        when (state) {
+                            Player.STATE_BUFFERING -> {
+                                bind?.progressBar?.visibility = View.VISIBLE
+                            }
 
-                                Player.STATE_READY,
-                                Player.STATE_ENDED,
-                                Player.STATE_IDLE -> {
-                                    bind?.progressBar?.visibility = View.GONE
-                                }
+                            Player.STATE_READY, Player.STATE_ENDED, Player.STATE_IDLE -> {
+                                bind?.progressBar?.visibility = View.GONE
                             }
                         }
-                    })
-                }
+                    }
+                })
+            }
+            // Set the initial resize mode to fit
+            toggleImmersiveMode()
         }
     }
 
@@ -192,6 +256,7 @@ class Home : BaseFragment<HomeBinding>(HomeBinding::inflate) {
 
     override fun onPause() {
         super.onPause()
+        // Pause playback when the fragment is paused to save resources
         if (this::player.isInitialized) {
             player.playWhenReady = false
             player.pause()
@@ -200,6 +265,7 @@ class Home : BaseFragment<HomeBinding>(HomeBinding::inflate) {
 
     override fun onResume() {
         super.onResume()
+        // Resume playback if the player is initialized
         if (this::player.isInitialized) {
             player.playWhenReady = true
             player.play()
@@ -209,13 +275,13 @@ class Home : BaseFragment<HomeBinding>(HomeBinding::inflate) {
 
     override fun onDestroy() {
         super.onDestroy()
+        // Release the player when the fragment is destroyed to free up resources
         player.release()
     }
 }
 
 
-
-
+@OptIn(UnstableApi::class)
 private fun Home.toggleImmersiveMode() {
     // Set the window to not fit system windows, allowing for immersive mode
     WindowCompat.setDecorFitsSystemWindows(requireActivity().window, false)
